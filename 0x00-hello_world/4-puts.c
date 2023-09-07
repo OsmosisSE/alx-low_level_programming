3c37557894d083cc005b93b777b06122660012d7
OsmosisSE <sirosmosis1@gmail.com>
2023-09-07 16:33:10 -0700

    Hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
 #include<stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ *Hello C
  */
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0); 
 }
