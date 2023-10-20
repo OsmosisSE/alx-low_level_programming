c0d3e70ecbbf987d4adcfa74b5b6d72f849b8b11
OsmosisSE <sirosmosis1@gmail.com>
2023-10-19 18:15:11 -0700

    updated

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
 
 /**
- * pre_main_message - Display a message before the main functionis executed.
- *
- * Description:
- * This function is automatically called before the amin function.
- * It displays a message to the console.
+ * premain - prints a sentence before the main.
+ * function is executed.
  */
-
-void pre_main_message(void)
+void premain(void)
 {
 	printf("You're beeat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
