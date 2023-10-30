b8b616fb2868dec4acf56abac019a9635a508f9f
OsmosisSE <sirosmosis1@gmail.com>
2023-10-30 16:39:33 -0700

    updated

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -20,31 +20,23 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (fd == -1)
 		return (0);
 
-	buffer = malloc(letters);
+	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 	{
-		close(fd);
+		free(buffer);
 		return (0);
 	}
 
 	bytes_read = read(fd, buffer, letters);
 	if (bytes_read == -1)
-	{
-		close(fd);
-		free(buffer);
 		return (0);
-	}
 
 	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-	if (bytes_written == -1 || bytes_written != bytes_read)
-	{
-		close(fd);
-		free(buffer);
+	if (bytes_written == -1 || bytes_read != bytes_written)
 		return (0);
-	}
 
-	close(fd);
 	free(buffer);
+	close(fd);
 
 	return (bytes_read);
 }
