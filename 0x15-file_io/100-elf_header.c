da3a6b55b7c2d2775a322cb3f6e694cfedd8e654
OsmosisSE <sirosmosis1@gmail.com>
2023-11-02 14:03:09 -0700

    updated

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -3,9 +3,8 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdio.h>
 #include <stdlib.h>
-
-#define EI_NIDENT 16
 
 void check_elf(unsigned char *e_ident);
 void print_magic(unsigned char *e_ident);
@@ -31,18 +30,19 @@ void check_elf(unsigned char *e_ident)
 	for (index = 0; index < 4; index++)
 	{
 		if (e_ident[index] != 127 &&
-				e_ident[index] != 'E' &&
-				e_ident[index] != 'L' &&
-				e_ident[index] != 'F' &&)
+		    e_ident[index] != 'E' &&
+		    e_ident[index] != 'L' &&
+		    e_ident[index] != 'F')
 		{
-			dprintf(STDERR_FILENO, "Error: NOt an ELF file\n");
+			dprintf(STDERR_FILENO, "Error: Not an ELF file\n");
+			exit(98);
 		}
 	}
 }
 
 /**
  * print_magic - Prints the magic numbers of an ELF header.
- * @e_ident: A pointer to an array containing the ELF maigc numbers.
+ * @e_ident: A pointer to an array containing the ELF magic numbers.
  *
  * Description: Magic numbers are separated by spaces.
  */
@@ -50,11 +50,16 @@ void print_magic(unsigned char *e_ident)
 {
 	int index;
 
-	dprint(STDOUT_FILENO, " Magic: ");
+	printf("  Magic:   ");
+
 	for (index = 0; index < EI_NIDENT; index++)
 	{
-		dprint(STDOUT_FILENO, index == EI_NIDENT - 1
-				? "%02x\n" : "%02x ", e_ident[index];
+		printf("%02x", e_ident[index]);
+
+		if (index == EI_NIDENT - 1)
+			printf("\n");
+		else
+			printf(" ");
 	}
 }
 
@@ -64,7 +69,7 @@ void print_magic(unsigned char *e_ident)
  */
 void print_class(unsigned char *e_ident)
 {
-	printf("  Class:            ");
+	printf("  Class:                             ");
 
 	switch (e_ident[EI_CLASS])
 	{
@@ -83,20 +88,46 @@ void print_class(unsigned char *e_ident)
 }
 
 /**
- * print_version - Prints the data of an ELF header.
+ * print_data - Prints the data of an ELF header.
  * @e_ident: A pointer to an array containing the ELF class.
+ */
+void print_data(unsigned char *e_ident)
+{
+	printf("  Data:                              ");
+
+	switch (e_ident[EI_DATA])
+	{
+	case ELFDATANONE:
+		printf("none\n");
+		break;
+	case ELFDATA2LSB:
+		printf("2's complement, little endian\n");
+		break;
+	case ELFDATA2MSB:
+		printf("2's complement, big endian\n");
+		break;
+	default:
+		printf("<unknown: %x>\n", e_ident[EI_CLASS]);
+	}
+}
+
+/**
+ * print_version - Prints the version of an ELF header.
+ * @e_ident: A pointer to an array containing the ELF version.
  */
 void print_version(unsigned char *e_ident)
 {
-	dprint(STDOUT_FILENO, " Version:         %d", e_ident[EI_VERSION]);
-	switch (e_ident[EI_VERSION);
+	printf("  Version:                           %d",
+	       e_ident[EI_VERSION]);
+
+	switch (e_ident[EI_VERSION])
 	{
-		case EV_CURRENT:
-			dprintf(STDOUT_FILENO, " (current)\n");
-			break;
-		default:
-			dprint(STDOUT_FILENO, "\n");
-			break;
+	case EV_CURRENT:
+		printf(" (current)\n");
+		break;
+	default:
+		printf("\n");
+		break;
 	}
 }
 
@@ -105,7 +136,7 @@ void print_version(unsigned char *e_ident)
  * @e_ident: A pointer to an array containing the ELF version.
  */
 void print_osabi(unsigned char *e_ident)
-	{
+{
 	printf("  OS/ABI:                            ");
 
 	switch (e_ident[EI_OSABI])
