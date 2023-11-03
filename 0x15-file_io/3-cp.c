a5d3a6b3f3054b0c2cb6c9cecdca8f31e0afe0d9
OsmosisSE <sirosmosis1@gmail.com>
2023-11-02 20:25:45 -0700

    updated

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,9 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdib.h>
 
-#define BUFFER_SIZE 1024
+char *create_buffer(char *file);
+void close_file(int fd);
 
 /**
 * main - Copies the content of one file to another.
@@ -13,33 +16,27 @@ int main(int argc, char *argv[])
 {
 	int fd_from, fd_to;
 	ssize_t _read, _written;
-	char buffer[BUFFER_SIZE];
+	char *buffer;
 
-	if (argc != 3)
+	if (argc != 3 || (fd_from = open(argv[1], O_RDONLY)) == -1 ||
+			(fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664)) == -1)
 	{
+		if (argc != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		return (97);
+		else if (fd_from == -1)
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		else
+		{
+			close(fd_from);
+			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
+		}
+		return (argc != 3 ? 97 : 98);
 	}
 
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		return (98);
-	}
-
-	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP);
-	if (fd_to == -1)
-	{
-		close(fd_from);
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
-		return (99);
-	}
-
-	while ((_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	while ((_read = read(fd_from, buffer, *buffer)) > 0)
 	{
 		_written = write(fd_to, buffer, _read);
-		if (_written == -1) 
+		if (_written == -1)
 		{
 			close(fd_from);
 			close(fd_to);
@@ -48,25 +45,17 @@ int main(int argc, char *argv[])
 		}
 	}
 
-	if (_read == -1)
+	if (_read == -1 || (close(fd_from) == -1) | (close(fd_to) == -1))
 	{
-		close(fd_from);
-		close(fd_to);
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		return (98);
-	}
+		if (_read == -1)
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		else
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", (close(fd_from) == -1) ? fd_to : fd_from);
+        return (_read == -1 ? 98 : 100);
+        }
 
-	if (close(fd_from) == -1 || close(fd_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", (fd_from == -1) ? fd_to : fd_from);
-		return (100);
-	}
-	
 	return (0);
 }
-
-char *create_buffer(char *file);
-void close_file(int fd);
 
 /**
  * create_buffer - Allocates 1024 bytes for a buffer.
