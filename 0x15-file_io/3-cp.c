e9fa49f82edaaeabe632e5d2c54958a507cb436d
OsmosisSE <sirosmosis1@gmail.com>
2023-11-03 20:15:42 -0700

    updated

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,97 +1,85 @@
 #include "main.h"
 #include <stdio.h>
-#include <stdib.h>
+#include <stdlib.h>
 
-char *create_buffer(char *file);
-void close_file(int fd);
+#define MAXSIZE 1024
 
 /**
-* main - Copies the content of one file to another.
-* @argc: The number of command-line arguments.
-* @argv: An array of command-line arguments.
-*
-* Return: 0 on success, or an exit code (97-100) on failure.
-*/
+ * __exit - prints error messages and exits with exit number
+ *
+ * @error: either the exit number or file descriptor
+ * @str: name of either file_in or file_out
+ * @fd: file descriptor
+ *
+ * Return: 0 on success
+ */
+int __exit(int error, char *str, int fd)
+{
+	switch (error)
+	{
+		case 97:
+			dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+			exit(error);
+		case 98:
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", "Original");
+			exit(error);
+		case 99:
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", "duplicate");
+			exit(error);
+		case 100:
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+			exit(error);
+		default:
+			return (0);
+	}
+}
+
+/**
+ * main - Copies the contents of a file to another file.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: 0 for success.
+ */
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to;
-	ssize_t _read, _written;
-	char *buffer;
+	int file_in, file_out;
+	int read_stat, write_stat;
+	int close_in, close_out;
+	char buffer[MAXSIZE];
 
-	if (argc != 3 || (fd_from = open(argv[1], O_RDONLY)) == -1 ||
-			(fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664)) == -1)
+	if (argc != 3)
+		__exit(97, NULL, 0);
+
+	file_in = open(argv[1], O_RDONLY);
+	if (file_in == -1)
+		__exit(98, argv[1], 0);
+
+	file_out = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
+	if (file_out == -1)
+		__exit(99, argv[2], 0);
+
+	while ((read_stat = read(file_in, buffer, MAXSIZE)) != 0)
 	{
-		if (argc != 3)
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		else if (fd_from == -1)
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		else
-		{
-			close(fd_from);
-			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
-		}
-		return (argc != 3 ? 97 : 98);
+		if (read_stat == -1)
+			__exit(98, argv[1], 0);
+
+		write_stat = write(file_out, buffer, read_stat);
+		if (write_stat == -1)
+			__exit(99, argv[2], 0);
 	}
 
-	while ((_read = read(fd_from, buffer, *buffer)) > 0)
-	{
-		_written = write(fd_to, buffer, _read);
-		if (_written == -1)
-		{
-			close(fd_from);
-			close(fd_to);
-			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
-			return (99);
-		}
-	}
+	close_in = close(file_in);
+	if (close_in == -1)
+		__exit(100, NULL, file_in);
 
-	if (_read == -1 || (close(fd_from) == -1) | (close(fd_to) == -1))
-	{
-		if (_read == -1)
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		else
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", (close(fd_from) == -1) ? fd_to : fd_from);
-        return (_read == -1 ? 98 : 100);
-        }
+	close_in = close(file_in);
+	if (close_in == -1)
+		__exit(100, NULL, file_in);
+
+	close_out = close(file_out);
+	if (close_out == -1)
+		__exit(100, NULL, file_out);
 
 	return (0);
-}
-
-/**
- * create_buffer - Allocates 1024 bytes for a buffer.
- * @file: The name of the file buffer is storing chars for.
- *
- * Return: A pointer to the newly-allocated buffer.
- */
-char *create_buffer(char *file)
-{
-	char *buffer;
-
-	buffer = malloc(sizeof(char) * 1024);
-
-	if (buffer == NULL)
-	{
-		dprintf(STDERR_FILENO,
-			"Error: Can't write to %s\n", file);
-		exit(99);
-	}
-
-	return (buffer);
-}
-
-/**
- * close_file - Closes file descriptors.
- * @fd: The file descriptor to be closed.
- */
-void close_file(int fd)
-{
-	int c;
-
-	c = close(fd);
-
-	if (c == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-		exit(100);
-	}
 }
