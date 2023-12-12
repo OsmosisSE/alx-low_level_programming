aa09231d98008d65a72ed469d776a476c4c6c79f
OsmosisSE <sirosmosis1@gmail.com>
2023-12-11 20:53:41 -0800

    updated

---
+++ 0x18-dynamic_libraries/cal.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+/**
+ * add - add two integers
+ * @a: first int
+ * @b: second int
+ * Return: Addition
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtract two integers
+ * @a: first int
+ * @b: second int
+ * Return: Subtraction
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mod - mod two integers
+ * @a: first int
+ * @b: second int
+ * Return: 
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
+
+/**
+ * mul - Multiply two integers
+ * @a: first int
+ * @b: second int
+ * Return: Multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - division two integers
+ * @a: first int
+ * @b: second int
+ * Return: division
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
